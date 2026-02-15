1ce5977deb86a24d6c62539f6796566ab0f35548
Your Name <you@example.com>
2026-02-15 23:34:30 +0100

    magic

--- components/ui/input.jsx
+++ components/ui/input.jsx
@@ -1,14 +1,14 @@
-import * as React from "react"
+import * as React from "react";
 
-import { cn } from "@/lib/utils"
+import { cn } from "@/lib/utils";
 
-function Input({
-  className,
-  type,
-  ...props
-}) {
+const Input = React.forwardRef(function Input(
+  { className, type, ...props },
+  ref
+) {
   return (
-    (<input
+    <input
+      ref={ref}
       type={type}
       data-slot="input"
       className={cn(
@@ -17,8 +17,9 @@ function Input({
         "aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive",
         className
       )}
-      {...props} />)
+      {...props}
+    />
   );
-}
+});
 
-export { Input }
+export { Input };
