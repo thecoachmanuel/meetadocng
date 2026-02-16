519cfe47139605a61bd3e66f66f0382128947e7d
Your Name <you@example.com>
2026-02-16 17:07:46 +0100

    settings updated

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 	images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "img.clerk.com",
-      },
-    ],
-  },
+		remotePatterns: [
+			{
+				protocol: "https",
+				hostname: "img.clerk.com",
+			},
+			{
+				protocol: "https",
+				hostname: "fvneejbbugpsgyezslxd.supabase.co",
+				pathname: "/storage/v1/object/public/site/**",
+			},
+		],
+	},
 };
 
 export default nextConfig;
