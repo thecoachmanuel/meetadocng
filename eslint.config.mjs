1b1ba2c0b714631f3510f36ab2726691f40ec38c
Your Name <you@example.com>
2026-02-18 00:37:53 +0100

    latest

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,14 +1,26 @@
-import nextCoreWebVitals from "eslint-config-next/core-web-vitals";
+import { FlatCompat } from "@eslint/eslintrc";
+import { fileURLToPath } from "url";
+import { dirname } from "path";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const compat = new FlatCompat({
+	baseDirectory: __dirname,
+});
 
 const eslintConfig = [
-  ...nextCoreWebVitals,
-  {
-    rules: {
-      "react-hooks/set-state-in-effect": "off",
-      "react-hooks/error-boundaries": "off",
-      "react/no-unescaped-entities": "off",
-    },
-  },
+	{
+		ignores: [".next/**", "prev_page.js"],
+	},
+	...compat.config({ extends: ["next/core-web-vitals"] }),
+	{
+		rules: {
+			"react-hooks/set-state-in-effect": "off",
+			"react-hooks/error-boundaries": "off",
+			"react/no-unescaped-entities": "off",
+		},
+	},
 ];
 
 export default eslintConfig;
